0a272062171c301af9fd1308f0d7a8398938c981
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-03 11:35:04 -0300

    exercicio_1

---
+++ Estrutura_Sequencial/exercicio_1.java
@@ -0,0 +1,8 @@
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+public class exercicio_1 {
+    public static void main(String[] args) {
+        System.out.println("Olá,mundo !!!");
+
+    }
+}
