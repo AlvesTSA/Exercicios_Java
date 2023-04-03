c7cf63f68a8b11f359e252c39314311ac81a9ec8
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-03 17:38:55 -0300

    exercicio_3

---
+++ Estrutura_Sequencial/exercicio_3.java
@@ -0,0 +1,23 @@
+//Faça um Programa que peça dois números e imprima a soma.
+import java.util.Scanner;
+
+public class exercicio_3 {
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+
+        float numero1 = scanner.nextFloat();
+
+        System.out.print("Digite o segundo número: ");
+
+        float numero2 = scanner.nextFloat();
+
+        float soma = numero1 + numero2;
+
+        System.out.print("A soma e igual a: " + soma);
+
+    }
+    
+}
