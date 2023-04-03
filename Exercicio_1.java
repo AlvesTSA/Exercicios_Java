c269e776ac9d21f18a39877bad4224dc9858c343
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-03 11:22:50 -0300

    exercicio_1

--- Exercicio_1.java
+++ Exercicio_1.java
@@ -1,11 +1,9 @@
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
 
 public class Exercicio_1 {
-
-   
     public static void main(String[] args) {
         System.out.println("Olá,mundo !!!");
 
-        /teste
     }
     
 }
