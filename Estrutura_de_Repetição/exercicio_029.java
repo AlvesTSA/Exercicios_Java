a6db8db9c6afeeb476636a582fa24a71997d63e4
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-12-19 16:48:36 -0300

    Create exercicio_029.java

---
+++ Estrutura_de_Repetição/exercicio_029.java
@@ -0,0 +1,36 @@
+/*29. O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
+
+Lojas Quase Dois - Tabela de preços 
+
+1 - R$ 1.99 
+
+2 - R$ 3.98
+
+...
+
+50 - R$ 99.50
+
+ */
+
+import java.util.Scanner;
+
+public class exercicio_029 {
+    
+    public static void main(String[] args){
+    
+        double soma = 0;
+        double preco = 1.99;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lojas Quase Dois - Tabela de preços");
+
+        for(int i = 1; i <= 50; i++){
+
+            soma += preco;
+            String somaformat = String.format("%.2f", soma);
+            System.out.println(i + "- R$ " + somaformat);
+        }
+        scanner.close();
+    }
+}
